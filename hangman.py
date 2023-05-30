53827ee65cd7a1b58a515dc74c6c671d730d6c9d
midhul <dhulmi3@gmail.com>
2023-05-30 15:58:58 +0530

    Assertion error cleared in maskword()

--- hangman.py
+++ hangman.py
@@ -1,5 +1,8 @@
 import random
 
+ALREADY_GUESSED = 0
+CORRECT = 1
+WRONG = 2
 
 def get_random_word(wordfile = "/usr/share/dict/words"):
     candidate_words = []
@@ -13,83 +16,66 @@ def get_random_word(wordfile = "/usr/share/dict/words"):
     return word
 a="/usr/share/dict/words"
 
-def masked_word(word,tried_letters):
+def maskword(secret_word, guesses):
+    op = []
+    for i in secret_word:
+        if i in guesses:
+            op.append(i)
+        else:
+            op.append("-")
+    return "".join(op)
 
-    display_word = ""
-    for letter in word:
-       if letter in tried_letters:
-            display_word += letter 
-       else:
-            display_word += "_"
-    return(display_word)
+def update_stats(secret_word, guesses, turns_remaining):
+    masked_word = maskword(secret_word, guesses)
+    guessed_letters = " ".join(guesses)
+    return f"""Secret word:{masked_word}
+Guesses : {guessed_letters}
+Remaining turns : {turns_remaining}"""
+
+def check(secret_word, guesses, turns_remaining, new_guess):
+    if new_guess in guesses:
+        return ALREADY_GUESSED, turns_remaining
+    else:
+        guesses.append(new_guess)
+        if new_guess in secret_word:
+            return CORRECT, turns_remaining
+        else:
+            return WRONG, turns_remaining-1
 
         
-
-def stats(sw,tried_letters,guess):
-    sw=get_random_word(a)
-    c="wrong guess"
-    b="already guessed"
-    g=""
-    tried_letters=[]
-    # new=tried_letters + [guess]
-    new1=[sw]+tried_letters
+def game_over(secret_word, guesses, turns_remaining):
+    if turns_remaining == 0:
+        return True, f"You lost! The word was {secret_word}"
+    masked = maskword(secret_word, guesses)
+    if "-" in masked:
+        return False, None
+    else:
+        return True, f"You Won! The word is {secret_word}"
+    
+    
+def main():
+    secret_word = get_random_word()
+    # print (secret_word)
+    guesses = []
+    turns_remaining = 8
     while True:
-     if guess in tried_letters:
-        return b
-     if guess not in new1:
-        return c
-     for i in new1:
-       if i in sw:
-          return 
-    
+        print (update_stats(secret_word, guesses, turns_remaining))
+        guess = input("Enter a letter ")
         
-     
-    
+        status, turns_remaining = check(secret_word, guesses, turns_remaining, guess)
+        if status == ALREADY_GUESSED:
+            print ("Already guessed")
+        
+        finished, message = game_over(secret_word, guesses, turns_remaining)
+        if message:
+            print (message)
+        if finished:
+            break
 
-    
 
-
-# def play_hangman():
-#     word = get_random_word(a)
-#     word = word.lower()
-#     guessed_letters = []
-#     tries = 6
-
-#     while tries > 0:
-#         guessed_word = ""
-#         for letter in word:
-#             if letter in guessed_letters:
-#                 guessed_word += letter
-#             else:
-#                 guessed_word += "_"
-
-#         print("\nGuessed Word:", guessed_word)
-#         print("Tries Left:", tries)
-
-#         if guessed_word == word:
-#             print("Congratulations! You guessed the word correctly!")
-#             break
-
-#         guess = input("Guess a letter: ").lower()
-
-#         if guess in guessed_letters:
-#             print("You already guessed that letter. Try again!")
-#         elif len(guess) != 1:
-#             print("Please enter a single letter.")
-#         elif not guess.isalpha():
-#             print("Please enter only alphabetic characters.")
-#         else:
-#             guessed_letters.append(guess)
-#             if guess in word:
-#                 print("Good guess!")
-#             else:
-#                 print("Wrong guess!")
-#                 tries -= 1
-
-#     else:
-#         print("\nSorry, you ran out of tries.")
-#         print("The word was:", word)
-
+if __name__ == "__main__":
+    main()
+               
 
    
 
